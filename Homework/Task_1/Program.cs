0a954b075263a216b46c15c2391bb160722b44b3
Kirill <cleidoic@bk.ru>
2022-11-15 05:08:29 +0300

    Added first task

--- Homework/Task_1/Program.cs
+++ Homework/Task_1/Program.cs
@@ -15,3 +15,61 @@
 8 4 4 2
 */
 
+int GetNumber(string message) {
+    bool isCorrect = false;
+    int result = 0;
+    Console.WriteLine(message);
+    while (!isCorrect) {
+        isCorrect = int.TryParse(Console.ReadLine(), out result);
+        if (!isCorrect)
+            Console.WriteLine("\nВвели не число или оно слишком большое. Введите заново: ");
+    }
+    return result;
+}
+
+int[,] GetArray(int rows, int columns) {
+    int[,] array = new int[rows, columns];
+    Random random = new Random();
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            array[i, j] = random.Next(-100, 100);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array) {
+    Console.WriteLine("\nПолучили матрицу:\n");
+    int row = array.GetLength(0);
+    int columns = array.GetLength(1);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < columns; j++) {
+            Console.Write("{0,5}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] OrderArray(int[,] array) {
+    Console.WriteLine("\nСортируем строки по убыванию...");
+    for (int row = 0; row < array.GetLength(0); row++) {
+        for (int i = 0; i < array.GetLength(1) - 1; i++) {
+            for (int j = i + 1; j < array.GetLength(1); j++) {
+                if (array[row, i] < array[row, j]) {
+                    int tmp = array[row, i];
+                    array[row, i] = array[row, j];
+                    array[row, j] = tmp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int rows = GetNumber("Введите число строк матрицы: ");
+int columns = GetNumber("Введите число столбцов матрицы: ");
+int[,] array = GetArray(rows, columns);
+
+PrintArray(array);
+OrderArray(array);
+PrintArray(array);
